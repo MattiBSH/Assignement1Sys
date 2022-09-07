5a93fb2fa5cb13e14d6ed512dbd34359678b7694
MattiBSH <cph-mh829@cphbusiness.dk>
2022-09-07 17:07:31 +0200

    fix

--- src/main/java/com/example/assignement1sys/Assignement1SysApplication.java
+++ src/main/java/com/example/assignement1sys/Assignement1SysApplication.java
@@ -1,13 +1,37 @@
 package com.example.assignement1sys;
 
+import mypackage.GeoIPServiceLocator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.xml.rpc.ServiceException;
+import java.rmi.RemoteException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 @SpringBootApplication
 public class Assignement1SysApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Assignement1SysApplication.class, args);
+    public static void main(String[] args) throws ServiceException, RemoteException {
+        GeoIPServiceLocator geoIPServiceLocator = new GeoIPServiceLocator();
+        String location =geoIPServiceLocator.getGeoIPServiceSoap().getIpLocation("176.20.21.192");
+        System.out.println(location);
     }
 
+    String generateMail(String ip, String name, String mail) throws ServiceException, RemoteException {
+        boolean isMale=false;
+        GeoIPServiceLocator geoIPServiceLocator = new GeoIPServiceLocator();
+        String location =geoIPServiceLocator.getGeoIPServiceSoap().getIpLocation(ip);
+        String countryID=getTagValue(location,"Country");
+        System.out.println(countryID);
+        return null;
+    }
+    public static String getTagValue(String xml, String tagName){
+        return xml.split("<"+tagName+">")[1].split("</"+tagName+">")[0];
+    }
+
+    String findOutGender(){
+
+        return "Male";
+    }
 }
