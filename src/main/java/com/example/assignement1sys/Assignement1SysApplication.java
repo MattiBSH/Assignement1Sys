f4eeb03500b15d6b60c8c08478e384f8eb913cd2
MattiBSH <cph-mh829@cphbusiness.dk>
2022-09-07 14:03:34 +0200

    first commit

---
+++ src/main/java/com/example/assignement1sys/Assignement1SysApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignement1sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignement1SysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignement1SysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assignement1sys/Assignement1SysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignement1sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignement1SysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
